f642bbadb7894cbab6c8f96108ef5c94588825a9
Данил Долгов <spectrefromthefuture@gmail.com>
2023-12-28 08:56:26 +0400

    almost done

--- main.py
+++ main.py
@@ -1,110 +1,100 @@
 import random as rd
 import numpy as np
+import statistics
+import pandas
 matrix = [[0.433, 0.389, 0.491, 0.618, 0.433, 1.000, 0.433],
           [0.853, 0.945, 1.000, 1.000, 0.844, 0.734, 0.853],
           [0.847, 0.447, 1.000, 0.847, 0.441, 0.971, 0.441],
           [0.467, 0.417, 0.667, 0.500, 0.500, 1.000, 0.417],
           [0.056, 0.031, 0.108, 0.142, 0.032, 1.000, 0.037]]
+
 priority = [[1, 0.1, 0.9, 1, 0.2],
             [0.1, 0.9, 0.2, 0.1, 1],
             [0.1, 0.2, 1, 0.8, 0.9],
             [rd.random(), rd.random(), rd.random(), rd.random(), rd.random(), rd.random()]]
-namePriority = ["ARDOR GAMING PORTAL AF24H1",
-                "DEXP DF22N1",
-                "MSI G274QPF",
-                "Xiaomi Mi Curved Gaming Monitor 34",
-                "DEXP DF24N2",
-                "Samsung Odyssey Ark G97NC",
-                "MSI PRO MP241X"]
-alt = 7
-crit = 5
 
-def calcMatrix(n):
-    for i in range(alt):
-        for j in range(crit):
-            matrix[j][i] *= priority[n - 1][j]
-
-def printRes(name, f):
-    print('\n' + name + ':')
-    for i in range(crit):
-        print(f'{i + 1} альтернатива ({namePriority[i]}): {f[i]}')
-    print(f'Оптимальный выбор: {f.index(max(f)) + 1}')
-
-def critLplsa():
-    f = []
-    for i in range(crit):
-        f.append(sum(matrix[i]) / 7)
-    printRes('Критерий Лапласа', f)
-
-def critValda():
-    f = []
-    for i in range(crit):
-        f.append(min(matrix[i]))
-    printRes('Критерий Вальда', f)
-
-def critSavja():
-    f = []
-    r = []
-    for i in range(crit):
-        r.append([])
-        for j in range(alt):
-            mx = max(matrix[i])
-            r[i].append(mx - matrix[i][j]) if matrix[i][j] != mx else None
-    for i in range(crit):
-        f.append(min(r[i]))
-    printRes('Критерий Сэвиджа', f)
-
-def critGurvica(a):
-    f = []
-    for i in range(crit):
-        f.append(max(matrix[i]) * a + min(matrix[i]) * (1 - a))
-    printRes('Критерий Гурвица (' + str(a) + ')', f)
+alternative_names = ["ARDOR GAMING PORTAL AF24H1",
+                    "DEXP DF22N1",
+                    "MSI G274QPF",
+                    "Xiaomi Mi Curved Gaming Monitor 34",
+                    "DEXP DF24N2",
+                    "Samsung Odyssey Ark G97NC",
+                    "MSI PRO MP241X"]
 
 
-def random_values(namePriority, order):
-    results = np.zeros(shape=(len(namePriority)))
+def get_laplace_criteria(matrix):
+    laplace_criteria = []
 
-    swap_probability = 0.1
-    swap_two_first_numbers = False
-    if rd.random() <= swap_probability:
-        swap_two_first_numbers = True
+    for alternative in matrix:
+        laplace_criteria.append(statistics.mean(alternative))
 
-    if swap_two_first_numbers and len(order) >= 2:
-        order[0], order[1] = order[1], order[0]
+    decision = laplace_criteria.index(max(laplace_criteria))
+    return laplace_criteria, decision
 
 
-    last_max = rd.uniform(0.6, 0.95)
-    for i in range(len(order)):
-        alter_id = order[i] - 1
-        if i == 0:
-            results[alter_id] = last_max
-        else:
-            last_max = rd.uniform(last_max * 0.5, last_max * 0.9)
-            results[alter_id] = last_max
+def get_wald_criteria(matrix):
+    wald_criteria = []
 
-    for i in range(len(results)):
-        if results[i] <= 0:
-            last_max = rd.uniform(last_max * 0.1, last_max)
-            results[i] = last_max
+    for alternative in matrix:
+        wald_criteria.append(min(alternative))
+    decision = wald_criteria.index(max(wald_criteria))
 
-    for i, name in enumerate(namePriority):
-        print(f"{i + 1} альтернатива ({name}): {round(results[i], rd.randint(3, 10))}")
-
-    max_id = 0
-    max_value = results[max_id]
-    for i in range(len(results)):
-        if results[i] > max_value:
-            max_value = results[i]
-            max_id = i
-    print(f"Оптимальный выбор: {max_id + 1}", end="\n\n")
+    return wald_criteria, decision
 
 
-exp = int(input('Номер эксперимента (1, 2, 3 или 4): ')) - 1
+def get_savage_criteria(matrix):
+    savage_criteria = []
 
-calcMatrix(exp)
-critLplsa()
-critValda()
-critSavja()
-critGurvica(0)
-critGurvica(0.5)
-critGurvica(1)
+    max_features = []
+    columns_count = len(matrix[0])
+    for j in range(columns_count):
+        max_feature = 0
+        for i in range(len(matrix)):
+            if matrix[i][j] > max_feature:
+                max_feature = matrix[i][j]
+        max_features.append(max_feature)
+
+    risks_matrix = matrix.copy()
+    for i in range(len(matrix)):
+        for j in range(len(matrix[i])):
+            risks_matrix[i][j] = max_features[j] - matrix[i][j]
+
+    for risks in risks_matrix:
+        savage_criteria.append(max(risks))
+
+    decision = savage_criteria.index(min(savage_criteria))
+    return savage_criteria, decision
+
+
+def get_hurwitz_criteria(matrix, optimism):
+    wald_criteria = []
+
+    for alternative in matrix:
+        wald_criteria.append(optimism * max(alternative) + (1 - optimism) * min(alternative))
+    decision = wald_criteria.index(max(wald_criteria))
+
+    return wald_criteria, decision
+
+
+def output_criteria_and_decision(names, criteria, decision):
+    for i in range(len(matrix)):
+        print(f"{i + 1}) {names[i]}: {criteria[i]}")
+    print(f"Оптимальный выбор: {decision + 1}")
+
+
+def calculate_and_output_criteria(criteria_function, criteria_name, optimism=-1.0):
+    if optimism >= 0:
+        criteria, decision = criteria_function(matrix, optimism)
+    else:
+        criteria, decision = criteria_function(matrix)
+    print(f"{criteria_name}:")
+    output_criteria_and_decision(alternative_names, criteria, decision)
+    print()
+
+
+if __name__ == '__main__':
+    calculate_and_output_criteria(get_laplace_criteria, "Критерий Лапласа")
+    calculate_and_output_criteria(get_wald_criteria, "Критерий Вальда")
+    calculate_and_output_criteria(get_savage_criteria, "Критерий Сэвиджа")
+    calculate_and_output_criteria(get_hurwitz_criteria, "Критерий Гурвица", optimism=0.5)
+
